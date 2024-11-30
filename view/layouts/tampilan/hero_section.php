5132ab172a0dba1aaa2aeee27ba92e24f9a5bf62
Hari Rizky Ardiantoro <155425837+Haruku24@users.noreply.github.com>
2024-11-30 14:28:59 +0700

    Delete view directory

--- view/layouts/tampilan/hero_section.php
+++
@@ -1,7 +0,0 @@
-<div class="hero-section mt-3">
-        <div class="container">
-            <h1>Bersama Kita Bisa Membantu</h1>
-            <p>Ayo berdonasi untuk mereka yang membutuhkan uluran tangan Anda.</p>
-            <button class="btn btn-danger">Donasi Sekarang</button>
-        </div>
-    </div>
